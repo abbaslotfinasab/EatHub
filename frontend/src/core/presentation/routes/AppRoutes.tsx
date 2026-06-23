db9f4e447e35f8a954122413ae45590e773553c2
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2026-06-23 17:54:55 +0330

    ssl added

--- frontend/src/core/presentation/routes/AppRoutes.tsx
+++ frontend/src/core/presentation/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 import {AppLayout} from "../components/layout/AppLayout";
 import {DashboardLayout} from "../components/layout/DashboardLayout";
