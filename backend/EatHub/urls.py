478f64b4a37c4b8f0e5246281478a7fd859f3f41
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2026-06-23 18:04:59 +0330

    api added to url.py

--- backend/EatHub/urls.py
+++ backend/EatHub/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("api/", include("api.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('products/', include('products.urls', namespace='products')),
